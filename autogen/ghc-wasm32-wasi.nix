0b34cb2639919774e1b54a09026fc27202280689
Cheng Shao <astrohavoc@gmail.com>
2022-05-24 12:54:36 +0000

    Update autogen

--- autogen/ghc-wasm32-wasi.nix
+++ autogen/ghc-wasm32-wasi.nix
@@ -2,7 +2,7 @@
 { fetchzip }:
 fetchzip {
   url =
-    "https://gitlab.haskell.org/api/v4/projects/1/jobs/1055651/artifacts/ghc-wasm32-wasi.tar.xz";
+    "https://gitlab.haskell.org/api/v4/projects/1/jobs/1059823/artifacts/ghc-wasm32-wasi.tar.xz";
   hash =
-    "sha512-Kw0w1lwkLKB4wm5rYfohSvuajkL3v5XjGgOELm9hV7VwTF+AuYd7X5/h/qZX9VGQDM00lVx0kLhU33MmW08KGA==";
+    "sha512-UbndGyijkSknIQ2zn/34TX/CKhyaTbMxggE0JQ3l6qyYuyr7ftRbRhjit++sPr7LtwQbCbOYk1o5PvuEkjafiw==";
 }
--- autogen/wasmtime.nix
+++ autogen/wasmtime.nix
@@ -2,8 +2,8 @@
 { fetchzip }:
 fetchzip {
   url =
-    "https://nightly.link/bytecodealliance/wasmtime/actions/artifacts/249744050.zip";
+    "https://nightly.link/bytecodealliance/wasmtime/actions/artifacts/250264761.zip";
   hash =
-    "sha512-xAND+VgSfk/iDFgScqdWUmwQVNb7WJWN7r5yxSqn1xGc5CM1M5bqD2tjAJiz3oWNnqskT4/6y2H+3My7794gkA==";
+    "sha512-ZcGS0csvewbOYEQ4SOBdA6nJteYLrqhzcs0Wmnh26jhdywaSc2dRA5LpugogYoyrh6l86aORH9hdc8/ZBRLokQ==";
   stripRoot = false;
 }
