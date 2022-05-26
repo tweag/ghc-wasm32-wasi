7b47e7272f27845a0aa05dc78fbc806952d19083
Cheng Shao <astrohavoc@gmail.com>
2022-05-26 16:29:36 +0000

    setup.sh: only install one bindist

--- README.md
+++ README.md
@@ -25,12 +25,13 @@ For Ubuntu 20.04 and similar glibc-based distros:
 $ ./setup.sh
 ...
 Everything set up in /home/runner/.ghc-wasm32-wasi.
-Call /home/runner/.ghc-wasm32-wasi/ghc-wasm32-wasi-gmp/bin/wasm32-wasi-ghc or /home/runner/.ghc-wasm32-wasi/ghc-wasm32-wasi-native/bin/wasm32-wasi-ghc to get started.
+Call /home/runner/.ghc-wasm32-wasi/ghc-wasm32-wasi/bin/wasm32-wasi-ghc to get started.
 ```
 
 Set `PREFIX` environment variable to customize where to set up things.
-The script requires `curl` and `unzip` to run.
+Set `BIGNUM_BACKEND` to `gmp`/`native` to specify the `ghc-bignum`
+backend. The script requires `curl` and `unzip` to run.
 
 Note that `setup.sh` is not standalone, it calls auto-generated
 scripts in `autogen/`, and you need to call it at the root directory
-of this repo's checkout. Set `PREFIX`
+of this repo's checkout.
--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+readonly BIGNUM_BACKEND="${BIGNUM_BACKEND:-gmp}"
 readonly PREFIX="${PREFIX:-$HOME/.ghc-wasm32-wasi}"
 readonly REPO=$PWD
 
@@ -24,30 +25,26 @@ cp lib/*.a "$PREFIX/wasi-sdk/share/wasi-sysroot/lib/wasm32-wasi"
 
 popd
 
-for bignum_backend in gmp native; do
+mkdir "$PREFIX/ghc-wasm32-wasi"
+"$REPO/autogen/ghc-wasm32-wasi-$BIGNUM_BACKEND.sh" | tar xJ --strip-components=1 -C "$PREFIX/ghc-wasm32-wasi"
+pushd "$PREFIX/ghc-wasm32-wasi"
+./configure \
+  AR="$PREFIX/wasi-sdk/bin/llvm-ar" \
+  CC="$PREFIX/wasi-sdk/bin/clang" \
+  CXX="$PREFIX/wasi-sdk/bin/clang++" \
+  LD="$PREFIX/wasi-sdk/bin/wasm-ld" \
+  RANLIB="$PREFIX/wasi-sdk/bin/llvm-ranlib" \
+  STRIP="$PREFIX/wasi-sdk/bin/llvm-strip" \
+  CONF_CC_OPTS_STAGE2="-O3 -mmutable-globals -mnontrapping-fptoint -mreference-types -msign-ext" \
+  CONF_CXX_OPTS_STAGE2="-fno-exceptions -O3 -mmutable-globals -mnontrapping-fptoint -mreference-types -msign-ext" \
+  CONF_GCC_LINKER_OPTS_STAGE2="-Wl,--error-limit=0,--growable-table,--stack-first -Wno-unused-command-line-argument" \
+  --host=x86_64-linux \
+  --target=wasm32-wasi
+make lib/settings
+./bin/wasm32-wasi-ghc-pkg recache
+popd
 
-  mkdir "$PREFIX/ghc-wasm32-wasi-$bignum_backend"
-  "$REPO/autogen/ghc-wasm32-wasi-$bignum_backend.sh" | tar xJ --strip-components=1 -C "$PREFIX/ghc-wasm32-wasi-$bignum_backend"
-  pushd "$PREFIX/ghc-wasm32-wasi-$bignum_backend"
-  ./configure \
-    AR="$PREFIX/wasi-sdk/bin/llvm-ar" \
-    CC="$PREFIX/wasi-sdk/bin/clang" \
-    CXX="$PREFIX/wasi-sdk/bin/clang++" \
-    LD="$PREFIX/wasi-sdk/bin/wasm-ld" \
-    RANLIB="$PREFIX/wasi-sdk/bin/llvm-ranlib" \
-    STRIP="$PREFIX/wasi-sdk/bin/llvm-strip" \
-    CONF_CC_OPTS_STAGE2="-O3 -mmutable-globals -mnontrapping-fptoint -mreference-types -msign-ext" \
-    CONF_CXX_OPTS_STAGE2="-fno-exceptions -O3 -mmutable-globals -mnontrapping-fptoint -mreference-types -msign-ext" \
-    CONF_GCC_LINKER_OPTS_STAGE2="-Wl,--error-limit=0,--growable-table,--stack-first -Wno-unused-command-line-argument" \
-    --host=x86_64-linux \
-    --target=wasm32-wasi
-  make lib/settings
-  ./bin/wasm32-wasi-ghc-pkg recache
-  popd
-
-  "$PREFIX/ghc-wasm32-wasi-$bignum_backend/bin/wasm32-wasi-ghc" --info
-
-done
+"$PREFIX/ghc-wasm32-wasi/bin/wasm32-wasi-ghc" --info
 
 echo "Everything set up in $PREFIX."
-echo "Call $PREFIX/ghc-wasm32-wasi-gmp/bin/wasm32-wasi-ghc or $PREFIX/ghc-wasm32-wasi-native/bin/wasm32-wasi-ghc to get started."
+echo "Call $PREFIX/ghc-wasm32-wasi/bin/wasm32-wasi-ghc to get started."
