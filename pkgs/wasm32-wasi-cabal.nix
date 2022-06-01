9bec036aeb651ae2afc7131081ddb5d40181cd1f
Cheng Shao <astrohavoc@gmail.com>
2022-06-01 13:51:34 +0000

    Add head.hackage config to nix wasm32-wasi-cabal

--- pkgs/wasm32-wasi-cabal.nix
+++ pkgs/wasm32-wasi-cabal.nix
@@ -1,5 +1,14 @@
 { cabal, ghc-wasm32-wasi, writeShellScriptBin, }:
 writeShellScriptBin "wasm32-wasi-cabal" ''
+  export CABAL_DIR="''${CABAL_DIR:-$HOME/.ghc-wasm32-wasi/.cabal}"
+
+  if [ ! -f "$CABAL_DIR/config" ]
+  then
+    mkdir -p "$CABAL_DIR"
+    cp ${../cabal.config} "$CABAL_DIR/config"
+    chmod u+w "$CABAL_DIR/config"
+  fi
+
   exec ${cabal}/bin/cabal \
     --with-compiler=${ghc-wasm32-wasi}/bin/wasm32-wasi-ghc \
     --with-hc-pkg=${ghc-wasm32-wasi}/bin/wasm32-wasi-ghc-pkg \
